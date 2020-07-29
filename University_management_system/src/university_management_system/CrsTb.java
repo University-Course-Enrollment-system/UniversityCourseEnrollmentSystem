ca804fdd3e18dfa23471dc63f72df2a2125a20ab
Asiri Senith <asirisenith@gmail.com>
2020-07-30 01:18:50 +0530

    Created CrsTb class

---
+++ University_management_system/src/university_management_system/CrsTb.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package university_management_system;
+
+/**
+ *
+ * @author O V A S Manjitha
+ */
+public class CrsTb {
+    
+}
