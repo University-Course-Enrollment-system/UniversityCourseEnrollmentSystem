2b02a6213667cd1510626ab27e87f433116cfe80
Asiri Senith <asirisenith@gmail.com>
2020-07-30 23:38:44 +0530

    import java.sql.ResultSet;

--- University_management_system/src/university_management_system/CrsTb.java
+++ University_management_system/src/university_management_system/CrsTb.java
@@ -7,7 +7,11 @@ package university_management_system;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author O V A S Manjitha
@@ -21,5 +25,31 @@ public class CrsTb {
     public CrsTb(){
     con = MyConnection.getconnection();
     }
-    
+    public void fillCrsTable(JTable table,String searchval){
+        try {
+           ps = con.prepareStatement("SELECT `subid`, `subname`, `faculty`, `credits`, `numassign`, `cost`, `semid`, `lecid`, `duration`, `location` FROM subjects WHERE CONCAT(  `subid` ,  `subname` ,  `faculty` ) LIKE ?");
+           ps.setString(1, "%"+searchval+"%");
+           ResultSet rs = ps.executeQuery();
+           DefaultTableModel model = (DefaultTableModel)table.getModel();
+           Object[] row;
+           while(rs.next()){
+           row = new Object[10];
+           row[0] = rs.getString(3);
+           row[1] = rs.getString(7);
+           row[2] = rs.getString(1);
+           row[3] = rs.getString(2);
+           row[4] = rs.getInt(4);
+           row[5] = rs.getInt(5);
+           row[6] = rs.getInt(6);
+           row[7] = rs.getString(8);
+           row[8] = rs.getInt(9);
+           row[9] = rs.getString(10);
+           model.addRow(row);
+           }
+       } catch (SQLException ex) {
+           //Logger.getLogger(StudentTb.class.getName()).log(Level.SEVERE, null, ex);
+           JOptionPane.showMessageDialog(null, ex.getMessage());
+       }
+   }
+
 }
