bdbb2aa01e2719d558a126a0edad8fb23bf8f434
Asiri Senith <asirisenith@gmail.com>
2020-07-31 19:47:29 +0530

    created fillCrsDetailTable method in CrsTb file

--- University_management_system/src/university_management_system/university_management_system/CrsTb.java
+++ University_management_system/src/university_management_system/university_management_system/CrsTb.java
@@ -174,4 +174,31 @@ public class CrsTb {
             System.out.println(ex.getMessage());
         }
     }
+    public void fillCrsDetailTable(JTable table,String stid){
+        try {
+           ps = con.prepareStatement("SELECT stsub.`semid` , stsub.`subjectid` , subjects.`subname` ,subjects.`type` ,subjects.`credits` , subjects.`numassign` , subjects.`cost` ,  subjects.`duration` , subjects.`lecid` , subjects.`location` FROM  `stsub` ,  `subjects` WHERE stsub.`subjectid` = subjects.`subid` AND stsub.`studentid`=? ORDER BY  `stsub`.`semid` ASC");
+           ps.setString(1, stid);
+           ResultSet rs = ps.executeQuery();
+           DefaultTableModel model = (DefaultTableModel)table.getModel();
+           Object[] row;
+           while(rs.next()){
+           row = new Object[10];
+           row[0] = rs.getString(1);
+           row[1] = rs.getString(2);
+           row[2] = rs.getString(3);
+           row[3] = rs.getString(4);
+           row[4] = rs.getInt(5);
+           row[5] = rs.getInt(6);
+           row[6] = rs.getInt(7);
+           row[7] = rs.getInt(8);
+           row[8] = rs.getString(9);
+           row[9] = rs.getString(10);
+            
+           model.addRow(row);
+           }
+       } catch (SQLException ex) {
+           //Logger.getLogger(StudentTb.class.getName()).log(Level.SEVERE, null, ex);
+           JOptionPane.showMessageDialog(null, ex.getMessage());
+       }
+   }
 }
