0b0ecd63178c13b039750becce2e87ad62ce08ed
Asiri Senith <asirisenith@gmail.com>
2020-08-02 01:13:19 +0530

    create fillstjtable method in StudentTb file

--- University_management_system/src/university_management_system/university_management_system/StudentTb.java
+++ University_management_system/src/university_management_system/university_management_system/StudentTb.java
@@ -20,4 +20,34 @@ public class StudentTb {
     public StudentTb(){
         con = MyConnection.getconnection();
     } 
+    public void fillstjtable(JTable table,String searchval){
+        
+        
+        try {
+            ps = con.prepareStatement("SELECT  `stid` ,  `fname` ,  `lname` ,  `dob` ,  `email` ,  `facname` ,  `enroldate` ,  `semid` ,  `zscore` ,  `qtype` FROM  `student` WHERE CONCAT(  `fname` ,  `lname` ,  `email` ) LIKE ?");
+            ps.setString(1, "%"+searchval+"%");
+            ResultSet rs = ps.executeQuery();
+            DefaultTableModel model = (DefaultTableModel)table.getModel();
+            Object[] row;
+            while(rs.next()){
+            row = new Object[10];
+            row[0] = rs.getString(1);
+            row[1] = rs.getString(2);
+            row[2] = rs.getString(3);
+            row[3] = rs.getString(4);
+            row[4] = rs.getString(5);
+            row[5] = rs.getString(6);
+            row[6] = rs.getString(7);
+            row[7] = rs.getString(8);
+            row[8] = rs.getString(9);
+            row[9] = rs.getString(10);
+            model.addRow(row);
+            }
+        } catch (SQLException ex) {
+            //Logger.getLogger(StudentTb.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    
+    }
+
 }
