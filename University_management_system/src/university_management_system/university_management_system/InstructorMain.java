b98a2fbc67bff2da5a77bb1e6c2b153ae2c09a9b
unknown <akilasithara3@gmail.com>
2020-08-01 10:23:47 +0530

    InstructorMain created

---
+++ University_management_system/src/university_management_system/university_management_system/InstructorMain.java
@@ -0,0 +1,27 @@
+
+public class InstructorMain extends javax.swing.JFrame {
+
+    /**
+     * Creates new form InstructorMain
+     */
+    InstructorTb it = new InstructorTb();
+    public InstructorMain() {
+        initComponents();
+        lbl_insid.setVisible(false);
+    }
+
+    
+    
+    
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton11;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton9;
+    private javax.swing.JDesktopPane jDesktopPane3;
+    private javax.swing.JPanel jPanel3;
+    public static javax.swing.JLabel lbl_insid;
+    public javax.swing.JLabel lbl_wellcome2;
+    public static javax.swing.JLabel test;
+    // End of variables declaration                   
+}
