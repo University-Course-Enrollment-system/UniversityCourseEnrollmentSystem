53d2cebbf02014bdd4cd8829ceef5e21664bb0ca
unknown <akilasithara3@gmail.com>
2020-08-01 13:50:19 +0530

    create LabAllocation method

---
+++ University_management_system/src/university_management_system/university_management_system/LabAllocation.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class LabAllocation extends javax.swing.JInternalFrame {
+
+    /**
+     * Creates new form LabAllocation
+     */
+    public LabAllocation() {
+        initComponents();
+    }
+
+  
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    public static javax.swing.JTable jTable1;
+    private javax.swing.JLabel lbl_totalcredits;
+    // End of variables declaration                   
+}
