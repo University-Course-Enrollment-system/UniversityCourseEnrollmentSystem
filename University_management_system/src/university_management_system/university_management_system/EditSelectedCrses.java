134de64be64e17f3092dc7cfcc974ad6d56e4b24
unknown <akilasithara3@gmail.com>
2020-08-01 10:10:34 +0530

    create the EditSelectedCrses method

---
+++ University_management_system/src/university_management_system/university_management_system/EditSelectedCrses.java
@@ -0,0 +1,22 @@
+
+public class EditSelectedCrses extends javax.swing.JInternalFrame {
+
+    /**
+     * Creates new form EditSelectedCrses
+     */
+    public EditSelectedCrses() {
+        initComponents();
+    }
+
+   
+
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    public static javax.swing.JTable jTable1;
+    private javax.swing.JLabel lbl_totalcredits;
+    // End of variables declaration                   
+}
