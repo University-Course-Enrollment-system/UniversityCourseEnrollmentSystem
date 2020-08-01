ae178542045edf1685d071ce3b62a3c862021b01
unknown <akilasithara3@gmail.com>
2020-08-01 13:42:48 +0530

    make the fillInsTable method

--- University_management_system/src/university_management_system/university_management_system/InstructorTb.java
+++ University_management_system/src/university_management_system/university_management_system/InstructorTb.java
@@ -25,5 +25,31 @@ public class InstructorTb {
         con = MyConnection.getconnection();
     }
 
+      public void fillInsTable(JTable table,String searchVal){
+        try {
+            ps = con.prepareStatement("SELECT `insid`, `fname`, `lname`, `email` FROM `instructor` WHERE CONCAT(  `fname` ,  `lname` ,  `email` ) LIKE ?");
+            ps.setString(1, "%"+searchVal+"%");
+  
+            ResultSet rs = ps.executeQuery();
+            DefaultTableModel model = (DefaultTableModel)table.getModel();
+            Object[] row;
+            while(rs.next()){
+            row = new Object[4];
+            row[0] = rs.getString(1);
+            row[1] = rs.getString(2);
+            row[2] = rs.getString(3);
+            row[3] = rs.getString(4);
+            
+            model.addRow(row);
+            }
+        } catch (SQLException ex) {
+            //Logger.getLogger(StudentTb.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        
+            
+         
+    }
+
     
 }
