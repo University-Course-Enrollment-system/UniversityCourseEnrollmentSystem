85841df8ea9729f16fecef6bbb719cdb2c0d2d8c
Asiri Senith <asirisenith@gmail.com>
2020-08-08 00:18:41 +0530

    created fillStrItems method fillCombo.java file

--- University_management_system/src/university_management_system/university_management_system/fillCombo.java
+++ University_management_system/src/university_management_system/university_management_system/fillCombo.java
@@ -6,26 +6,40 @@ import java.sql.SQLException;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 
- 
 public class fillCombo {
-   Connection con = null;
-   PreparedStatement ps = null;
-   ResultSet rs = null;
-   
-   public fillCombo(){
-   con = MyConnection.getconnection();
-   }
-   public void fillcombo1(){
-       try {
-           ps = con.prepareStatement("SELECT * FROM `lecture`");
-           rs = ps.executeQuery();
-           while(rs.next()){
-           String id = rs.getString(1);
-           AddCourse.combLecId.addItem(id);
-           
-           }
-       } catch (SQLException ex) {
-           JOptionPane.showMessageDialog(null, ex.getMessage());
-       }
-   }
+    Connection con = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public fillCombo() {
+        con = MyConnection.getconnection();
+    }
+
+    public void fillcombo1() {
+        try {
+            ps = con.prepareStatement("SELECT * FROM `lecture`");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String id = rs.getString(1);
+                AddCourse.combLecId.addItem(id);
+
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+
+    public void fillStrItems(JComboBox combo, String q, int Index) {
+        try {
+            ps = con.prepareStatement(q);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String id = rs.getString(Index);
+                combo.addItem(id);
+
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
 }
